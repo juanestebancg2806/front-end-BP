eb608078d42d732cd048286e6874bd1c09e7cf19
Andrés Achury <achuryjaramillo@gmail.com>
2023-02-01 16:35:08 -0500

    config: jest vite testing-library typescript setup

---
+++ tests/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+describe("Example tests", () => {
+  test("Renders main page correctly", () => {
+    const number1 = 2;
+    const number2 = 3;
+    const result = number1 + number2;
+
+    expect(result).toBe(10);
+  });
+});
