ef0ecf3127b910e0f8c4174ba4476fea06b9d921
Juan Esteban Cardona <juan.garcia@bld.ai>
2023-02-02 14:11:10 -0500

    complete dependencies install and configuration of prettier/eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'eslint-config-prettier' //with this if there is a rule confusion with prettier and eslint, prettier override the rule
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    'no-unused-vars': 'warning'
+  }
+}
