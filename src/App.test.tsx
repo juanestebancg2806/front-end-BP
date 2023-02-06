9c68df10f5833c32d727c41cf5b0c13562963594
Juan Esteban Cardona <juan.garcia@bld.ai>
2023-02-06 16:48:16 -0500

    complete configuration

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+describe("Example tests", () => {
+	test("Renders main page correctly", () => {
+		const number1 = 2;
+		const number2 = 3;
+		const result = number1 + number2;
+		expect(result).toBe(5);
+	});
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-
-const a = 4;
+import logo from "./assets/images/BP-logo.svg";
 
 const App: React.FC = () => {
 	const [count, setCount] = useState(0);
-
 	return (
 		<div className="App">
 			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+				<img src={logo} className="logo" alt="BP logo" />
 			</div>
-			<h1>Vite + React</h1>
+			<h1>Website under development</h1>
 			<div className="card">
 				<button
 					onClick={() => {
 						setCount((count) => count + 1);
 					}}
 				>
-					count is {count}
+					Click here to count: {count}
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</div>
 	);
 };
